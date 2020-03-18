1c12471cb7f9c943bfe1d71c245fc40c7bbed345
itxKAE <55869051+itxKAE@users.noreply.github.com>
2020-03-19 05:22:18 +0800

    error at line 585 of views.py

--- mapper/mapper/urls.py
+++ mapper/mapper/urls.py
@@ -6,8 +6,8 @@ from mapper import views
 
 urlpatterns = [
 	path('index/', views.index),
-	#path('result/', TemplateView.as_view(template_name='route.html')),
 	path('dijskra/', TemplateView.as_view(template_name='dijskra_route.html')),
 	path('pdijskra/', TemplateView.as_view(template_name='pdijskra_route.html')),
 	path('astar/', TemplateView.as_view(template_name='astar_route.html')),
+	path('bus_route/', TemplateView.as_view(template_name='bus_route.html')),
 ]
