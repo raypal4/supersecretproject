ba25141f375866e0724a0e098730dcb26f621edf
Nicholas Koh <nicholaskohwx@gmail.com>
2020-03-18 15:08:03 +0800

    Combined and can print our bug routing, but haven't plot on map

--- nic testing/osmFileTest.py
+++ nic testing/osmFileTest.py
@@ -7,6 +7,8 @@ import heapq as heapq
 import geopy.distance
 from shapely.geometry import Point, LineString
 from itertools import count
+import json
+import heapq
 
 from manualPatch.pois import *
 
@@ -17,7 +19,7 @@ def weightCalc(G, weight):
 		return lambda u, v, d: min(attr.get(weight, 1) for attr in d.values())
 	return lambda u, v, data: data.get(weight, 1)
 
-def heuristic(u, v):
+def heuristic(G, u, v):
 	u = (G.nodes[u]['y'], G.nodes[u]['x'])
 	v = (G.nodes[v]['y'], G.nodes[v]['x'])
 	return geopy.distance.distance(u, v).km
@@ -25,8 +27,7 @@ def heuristic(u, v):
 
 def astar_path(G, source, target, weight='weight'):
 	if source not in G or target not in G:
-		msg = f"Either source {source} or target {target} is not in G"
-		raise nx.NodeNotFound(msg)
+		raise nx.NodeNotFound(f"Either source {source} or target {target} is not in G")
 
 	push = heapq.heappush
 	pop = heapq.heappop
@@ -66,15 +67,52 @@ def astar_path(G, source, target, weight='weight'):
 				if qcost <= ncost:
 					continue
 			else:
-				h = heuristic(neighbor, target)
+				h = heuristic(G, neighbor, target)
 			enqueued[neighbor] = ncost, h
 			push(queue, (ncost + h, next(c), neighbor, ncost, curnode))
 
 	raise nx.NetworkXNoPath(f"Node {target} not reachable from {source}")
 
-def bus(start, end):
+def bfs(graph, start, end):
+	cost_per_transfer = 5
+	cost_per_stop = 1
+	seen = set()
+	# maintain a queue of paths
+	queue = []
+	# push the first path into the queue
+	heapq.heappush(queue, (0, 0, 0, [(start, None)]))
+	while queue:
+		# get the first path from the queue
+		(curr_cost, curr_distance, curr_transfers, path) = heapq.heappop(queue)
+
+		# get the last node from the path
+		(node, curr_service) = path[-1]
+
+		# path found
+		if node == end:
+			return (curr_cost, curr_distance, curr_transfers-1, path)
+
+		if (node, curr_service) in seen:
+			continue
+
+		seen.add((node, curr_service))
+		# enumerate all adjacent nodes, construct a new path and push it into the queue
+		for (adjacent, service), distance in graph.get(node, {}).items():
+			new_path = list(path)
+			new_path.append((adjacent, service))
+			new_distance = curr_distance + distance
+			new_cost = distance + curr_cost
+			new_transfers = curr_transfers
+			if curr_service != service:
+				new_cost += cost_per_transfer
+				new_transfers += 1
+			new_cost += cost_per_stop
+
+			heapq.heappush(queue, (new_cost, new_distance, new_transfers, new_path))
+
+def bus(busGraph, start, end):
 	tags = {
-	    'highway':'bus_stop',
+		'highway':'bus_stop',
 	}
 
 	words_rep = {
@@ -101,35 +139,87 @@ def bus(start, end):
 	startX = start[1]
 	startY = start[0]
 	bus = pois_from_polygon(box(startX-0.008999,startY-0.000094, startX+0.008999, startY+0.000094), tags=tags)
-	print(bus.columns)
-	
+	# print(bus.columns)
+	busStopStart = None
+
 	for name in bus["name"]:
-		print(name)
 		name = name.lower()
 		for word, initial in words_rep.items():
 			name = name.replace(word, initial)
-		name = name.title()
-		print(name)
+		busStopStart = name.title()
+		print(busStopStart)
 
 	endX = end[1]
 	endY = end[0]
 
 	bus = pois_from_polygon(box(endX-0.008999,endY-0.000094, endX+0.008999, endY+0.000094), tags=tags)
-	print(bus.columns)
-
+	# print(bus.columns)
+	busStopEnd = None
 	for name in bus["name"]:
-		print(name)
 		name = name.lower()
 		for word, initial in words_rep.items():
 			name = name.replace(word, initial)
-		name = name.title()
-		print(name)
+		busStopEnd = name.title()
+		print(busStopEnd)
 
+	(cost, distance, transfers, path) = bfs(busGraph, stop_desc_map[busStopStart]["BusStopCode"], stop_desc_map[busStopEnd]["BusStopCode"])
+	for code, service in path:
+		print(service, stop_code_map[code]["Description"])
+	print (len(path)-1, "stops")
+	print ("cost", cost)
+	print ("distance", distance, "km")
+	print ("transfers", transfers)
 	# STORE START AND END BUS STOPS THEN THROW INTO THE BUS ROUTING FUNCTION
 
-
-ox.config(log_console=True)
+# INIT
+# ox.config(log_console=True)
+print("Loading OSM")
 graph = ox.graph_from_file("punggol.osm", bidirectional=False, simplify=True, retain_all=False)
+
+print("Loading JSON")
+stops = json.loads(open("stops.json").read())
+services = json.loads(open("services.json").read())
+routes = json.loads(open("routes.json").read())
+
+print("Initializing tables")
+stop_desc_map = {stop["Description"]: stop for stop in stops}
+stop_code_map = {stop["BusStopCode"]: stop for stop in stops}
+
+print("Creating bus route map")
+routes_map = {}
+
+for route in routes:
+	key = (route["ServiceNo"], route["Direction"])
+	if key not in routes_map:
+		routes_map[key] = []
+	# hack around broken data
+	if (route["StopSequence"] == 4
+			and route["Distance"] == 9.1
+			and key == ("34", 1)):
+		route["StopSequence"] = 14
+	routes_map[key] += [route]
+
+
+print("Initializing Graph")
+busGraph = {}
+for service, path in routes_map.items():
+	# hack around broken data
+	path.sort(key = lambda r: r["StopSequence"])
+	for route_index in range(len(path) - 1):
+		key = path[route_index]["BusStopCode"]
+		if key not in busGraph:
+			busGraph[key] = {}
+		curr_route_stop = path[route_index]
+		next_route_stop = path[route_index + 1]
+		curr_distance = curr_route_stop["Distance"] or 0
+		next_distance = next_route_stop["Distance"] or curr_distance
+		distance = next_distance - curr_distance
+		assert distance >= 0, (curr_route_stop, next_route_stop)
+		curr_code = curr_route_stop["BusStopCode"]
+		next_code = next_route_stop["BusStopCode"]
+		busGraph[curr_code][(next_code, service)] = distance
+
+
 
 # start = ox.geocode("Singapore, Punggol MRT")
 # end = ox.geocode("singapore, Cove Stn")
@@ -154,7 +244,7 @@ for i in nodepath:
 route_nodes = nodes.loc[nodepath]
 # print(route_nodes)
 
-bus(start, end)
+bus(busGraph, start, end)
 
 graph_projected = ox.project_graph(graph)
 # ox.plot.plot_graph(graph_projected)
