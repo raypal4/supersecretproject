b9a5037c49f80547fad4815d9ceccd21fd7796b7
Nicholas Koh <nicholaskohwx@gmail.com>
2020-03-23 15:51:30 +0800

    bus walking fixed

--- bus_main.py
+++ bus_main.py
@@ -2,6 +2,7 @@ import folium
 import osmapi as osm
 import osmnx as ox
 from shapely import geometry, ops
+import geopy.distance
 
 from bus_functions import *
 
@@ -9,9 +10,12 @@ print("Loading OSM")
 graph = ox.graph_from_file("punggol.osm", bidirectional=True, simplify=True, retain_all=False)
 
 start = ox.geocode("punggol, singapore")
-end = ox.geocode("Blk 612B, Punggol Drive, Punggol, Northeast, 823612, Singapore")
+# end = ox.geocode("Punggol Blk 178A, singapore")
+end = (1.39702,103.91041)
 print("Found a starting node", start)
 print("Found a ending node", end)
+
+# graph = ox.graph_from_point(start, distance=5000, network_type='all')
 
 start_node = ox.get_nearest_node(graph, start)
 end_node = ox.get_nearest_node(graph, end)
@@ -26,8 +30,25 @@ if pathcheck[1] == 0:
 	startStopCoords = pathcheck[2]
 	endStopCoords = pathcheck[3]
 
+	print(startStopCoords)
+	print(endStopCoords)
+
+	# start_Bus = ox.get_nearest_nodes(graph, [startStopCoords[1]], [startStopCoords[0]])
+	# end_Bus = ox.get_nearest_nodes(graph, [endStopCoords[1]], [endStopCoords[0]])
+	
+	# for startwalk in start_Bus:
+	# 	pathToBusstop = astar_path(graph, start_node, startwalk)
+	# for endwalk in end_Bus:
+	# 	pathFromBusstop = astar_path(graph, endwalk, end_node)
+
+
 	start_Bus = ox.get_nearest_node(graph, startStopCoords)
 	end_Bus = ox.get_nearest_node(graph, endStopCoords)
+
+	# geom, u, v = ox.get_nearest_edge(graph, startStopCoords)
+	# start_Bus = min((u, v), key=lambda n: ox.great_circle_vec(startStopCoords[0], startStopCoords[1], graph.nodes[n]['y'], graph.nodes[n]['x']))
+	# geom, u, v = ox.get_nearest_edge(graph, endStopCoords)
+	# end_Bus = min((u, v), key=lambda n: ox.great_circle_vec(endStopCoords[0], endStopCoords[1], graph.nodes[n]['y'], graph.nodes[n]['x']))
 
 	pathToBusstop = astar_path(graph, start_node, start_Bus)
 	pathFromBusstop = astar_path(graph, end_Bus, end_node)
@@ -35,7 +56,12 @@ if pathcheck[1] == 0:
 	latlontobus = []
 	latlonfrombus = []
 
+	# walk from start to bus start
+	startbuscoord = (graph.nodes[start_Bus]['y'], graph.nodes[start_Bus]['x'])
 	prev = None
+	splice = None
+	ptr = 0
+	temp = float("Infinity")
 	for item in pathToBusstop:
 		if prev is None:
 			prev = item
@@ -43,13 +69,26 @@ if pathcheck[1] == 0:
 			try:
 				line = graph[prev][item][0]["geometry"]
 				for point in list(line.coords):
+					if splice is None:
+						splice = ptr
+						temp =  geopy.distance.distance(startbuscoord, (point[1], point[0])).km
+					elif geopy.distance.distance(startbuscoord, (point[1], point[0])).km < temp:
+						splice = ptr
+						temp = geopy.distance.distance(startbuscoord, (point[1], point[0])).km
 					latlontobus.append((point[1], point[0]))
+					ptr += 1
 			except:
 				pass
 			finally:
 				prev = item
+	latlontobus = latlontobus[:splice+1]
 
+	# walk for bus end to dst
+	endbuscoord = (graph.nodes[end_Bus]['y'], graph.nodes[end_Bus]['x'])
 	prev = None
+	splice = None
+	ptr = 0
+	temp = float("Infinity")
 	for item in pathFromBusstop:
 		if prev is None:
 			prev = item
@@ -57,11 +96,19 @@ if pathcheck[1] == 0:
 			try:
 				line = graph[prev][item][0]["geometry"]
 				for point in list(line.coords):
+					if splice is None:
+						splice = ptr
+						temp =  geopy.distance.distance(endbuscoord, (point[1], point[0])).km
+					elif geopy.distance.distance(endbuscoord, (point[1], point[0])).km < temp:
+						splice = ptr
+						temp = geopy.distance.distance(endbuscoord, (point[1], point[0])).km
 					latlonfrombus.append((point[1], point[0]))
+					ptr += 1
 			except:
 				pass
 			finally:
 				prev = item
+	latlonfrombus = latlonfrombus[:splice+1]
 
 
 	path = pathcheck[0]
@@ -130,6 +177,7 @@ if pathcheck[1] == 0:
 		folium.Marker(location=loc, popup='Bus stop number:' + str(code),
 					  icon=folium.Icon(color='green', icon='bus', prefix='fa')).add_to(m)
 	folium.PolyLine(line, color="red", weight=2.5, opacity=1).add_to(m)
+
 
 	# start point to start busstop
 	folium.PolyLine([start, latlontobus[0]], color="blue", weight=2.5, opacity=1, dasharray="4").add_to(m)
