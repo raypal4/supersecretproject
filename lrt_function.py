c92ef01ab1504f72f707e16005c64ec853c6fe21
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2020-03-23 17:40:00 +0800

    changes

--- lrt_function.py
+++ lrt_function.py
@@ -16,212 +16,65 @@ print("Creating lrt route map")
 routes_map = {}
 
 for route in routes:
-   key = (route["Loop"], route["Direction"])
-   if key not in routes_map:
-       routes_map[key] = []
-   routes_map[key] += [route]
+    key = (route["Loop"], route["Direction"])
+    if key not in routes_map:
+        routes_map[key] = []
+    routes_map[key] += [route]
 
 print("Initializing Graph")
 lrtGraph = {}
-for loop, path in routes_map.items():
-    for route_index in range(len(path) - 1):
-        key = path[route_index]["Description"]
+
+for item in routes_map:
+    for index in range(len(routes_map[item])):
+        order = routes_map[item][index]["Distance"]
+        stationName = routes_map[item][index]["Description"]
+        direction = routes_map[item][index]["Direction"]
+        loop = routes_map[item][index]["Loop"]
+        lat = routes_map[item][index]["Latitude"]
+        lon = routes_map[item][index]["Longitude"]
+        key = (loop, direction)
         if key not in lrtGraph:
-            lrtGraph[key] = {}
-        curr_route_stop = path[route_index]
-        next_route_stop = path[route_index + 1]
-        curr_distance = curr_route_stop["Distance"] or 0
-        next_distance = next_route_stop["Distance"] or curr_distance
-        distance = next_distance - curr_distance
-        assert distance >= 0, (curr_route_stop, next_route_stop)
-        curr_code = curr_route_stop["Description"]
-        next_code = next_route_stop["Description"]
-        lrtGraph[curr_code][(next_code, loop)] = distance
+            lrtGraph[key] = []
+        lrtGraph[(loop, direction)] += [(order, stationName, lat, lon)]
 
 
-def bfs(graph, start, end):
-  cost_per_transfer = 5
-  cost_per_stop = 1
-  seen = set()
-  # maintain a queue of paths
-  queue = []
-  # push the first path into the queue
-  heapq.heappush(queue, (0, 0, 0, [(start, None)]))
-  while queue:
-      # get the first path from the queue
-      (curr_cost, curr_distance, curr_transfers, path) = heapq.heappop(queue)
+def LrtBFS(graph, start, end):
+    for item in graph:
+        print(item, graph[item], "\n")
 
-      # get the last node from the path
-      (node, curr_service) = path[-1]
 
-      # path found
-      if node == end:
-          return curr_cost, curr_distance, curr_transfers - 1, path
-
-      if (node, curr_service) in seen:
-          continue
-
-      seen.add((node, curr_service))
-      # enumerate all adjacent nodes, construct a new path and push it into
-      # the queue
-      for (adjacent, service), distance in graph.get(node, {}).items():
-          new_path = list(path)
-          new_path.append((adjacent, service))
-          new_distance = curr_distance + distance
-          new_cost = distance + curr_cost
-          new_transfers = curr_transfers
-          if curr_service != service:
-              new_cost += cost_per_transfer
-              new_transfers += 1
-          new_cost += cost_per_stop
-
-          heapq.heappush(
-              queue, (new_cost, new_distance, new_transfers, new_path))
+LrtBFS(lrtGraph, "Cove Station", 6)
 
 
 def lrt(lrtGraph, graph, start, end, start_node, end_node):
-  tags = {
-      'highway': 'bus_stop',
-      'building': 'train_station'
-  }
+    tags = {
+        'highway': 'bus_stop',
+        'building': 'train_station'
+    }
 
-  words_rep = {
-      "avenue": "ave",
-      "block": "blk",
-      "boulevard": "blvd",
-      "central": "ctrl",
-      "close": "cl",
-      "crescent": "cres",
-      "drive": "dr",
-      "expressway": "e'way",
-      "highway": "hway",
-      "industrial park": "ind park",
-      "mount": "mt",
-      "place": "pl",
-      "ring road": "ring rd",
-      "road": "rd",
-      "service road": "service rd",
-      "square": "sq",
-      "station": "stn",
-      "street": "st",
-      "punggol stn/waterway point": "punggol stn",
-      "opposite": "opp",
-      "primary": "pr",
-      "school": "sch",
-      "before": "bef",
-      "after": "aft"
-  }
-  # flag to check if there is a possible bus route (0 if have, 1 if loc is walkable)
-  lrtflag = 0
-
-  startlat = start[0]
-  startLon = start[1]
-  R = 6378137
-  dn = 250
-  de = 250
-  dLat = dn / R
-  dLon = de / (R * math.cos(math.pi * startlat / 180))
-  maxstartLat = startlat + dLat * 180 / math.pi
-  maxstartLon = startLon + dLon * 180 / math.pi
-  dn = -250
-  de = -250
-  dLat = dn / R
-  dLon = de / (R * math.cos(math.pi * startlat / 180))
-  minstartLat = startlat + dLat * 180 / math.pi
-  minstartLon = startLon + dLon * 180 / math.pi
-
-  lrt = pois_from_polygon(
-      box(minstartLon, minstartLat, maxstartLon, maxstartLat), tags=tags)
-  # print(bus.columns)
-  lrtStartName = []                                             
-  # if there is no start bus stop within 1km, person should default to walk --------------- test
-  if lrt.empty:
-      lrtflag = 1
-      return [astar_path(graph, start_node, end_node), lrtflag]
-
-  print("\nPossible Starting Stops:")
-  for name in lrt["name"]:
-      if isinstance(name, str):
-          name = name.lower()
-          for word, initial in words_rep.items():
-               name = name.replace(word, initial)
-          lrtStartName.append(name.title())
-  print(lrtStartName)
-
-  endlat = end[0]
-  endLon = end[1]
-  R = 6378137
-  dn = 250
-  de = 250
-  dLat = dn / R
-  dLon = de / (R * math.cos(math.pi * endlat / 180))
-  maxendLat = endlat + dLat * 180 / math.pi
-  maxendLon = endLon + dLon * 180 / math.pi
-  dn = -250
-  de = -250
-  dLat = dn / R
-  dLon = de / (R * math.cos(math.pi * endlat / 180))
-  minendLat = endlat + dLat * 180 / math.pi
-  minendLon = endLon + dLon * 180 / math.pi
-
-  lrt = pois_from_polygon(
-      box(minendLon, minendLat, maxendLon, maxendLat), tags=tags)
-  lrtEndName = []
-
-  print("\nPossible Ending Stops:")
-  for name in lrt["name"]:
-      if isinstance(name, str):
-          name = name.lower()
-          for word, initial in words_rep.items():
-              name = name.replace(word, initial)
-          lrtEndName.append(name.title())
-  print(lrtEndName, "\n")
-
-  startStation = {}
-  for stop in lrtStartName:
-      startStation.update(
-          dict(filter(lambda item: stop in item[0], stop_desc_map.items())))
-  endStation = {}
-  for stop in lrtEndName:
-      endStation.update(
-          dict(filter(lambda item: stop in item[0], stop_desc_map.items())))
-
-  results = []
-  for x in startStation:
-      for y in endStation:
-          if startStation[x]["Description"] != endStation[y]["Description"]:
-              results.append(
-                  bfs(lrtGraph, startStation[x]["Description"], endStation[y]["Description"]))
-          else:
-              print("same start and end stop:", startStation[x]["Description"])
-              lrtflag = 1
-              return [astar_path(graph, start_node, end_node), lrtflag]
-
-  cheapest = None
-  cheapestCost = float("Infinity")
-
-  for cost, distance, transfers, path in results:
-      # print(cost, cheapestCost)
-      if cost < cheapestCost:
-          cheapest = (cost, distance, transfers, path)
-          cheapestCost = cost
-
-  cheapestStopsArray = []
-
-  print("CHEAPEST ROUTE: ", cheapest, "\n")
-  print("----------------------ROUTE DESCRIPTION-----------------------")
-  if cheapest is not None:
-      cost, distance, transfers, path = cheapest
-      for code, service in path:
-        print(service, stop_desc_map[code]["Description"])
-        cheapestStopsArray.append(
-            (stop_desc_map[code]["Latitude"], stop_desc_map[code]["Longitude"]))
-      print("--------------------------------------------------------------")
-      print("Number of stops: ", len(path) - 1)
-      print("cost: ", cost)
-      print("distance: ", distance, "stops")
-      print("transfers: ", transfers)
-
-      startStop = cheapestStopsArray[0]
-      endStop = cheapestStopsArray[-1]
-      return [path, lrtflag, startStop, endStop]
+    words_rep = {
+        "avenue": "ave",
+        "block": "blk",
+        "boulevard": "blvd",
+        "central": "ctrl",
+        "close": "cl",
+        "crescent": "cres",
+        "drive": "dr",
+        "expressway": "e'way",
+        "highway": "hway",
+        "industrial park": "ind park",
+        "mount": "mt",
+        "place": "pl",
+        "ring road": "ring rd",
+        "road": "rd",
+        "service road": "service rd",
+        "square": "sq",
+        "station": "stn",
+        "street": "st",
+        "punggol stn/waterway point": "punggol stn",
+        "opposite": "opp",
+        "primary": "pr",
+        "school": "sch",
+        "before": "bef",
+        "after": "aft"
+    }
--- lrt_main.py
+++ lrt_main.py
@@ -5,7 +5,8 @@ import osmnx as ox
 from lrt_function import *
 
 print("Loading OSM")
-graph = ox.graph_from_file("punggol.osm", bidirectional=True, simplify=True, retain_all=False)
+graph = ox.graph_from_file(
+    "punggol.osm", bidirectional=True, simplify=True, retain_all=False)
 
 start = ox.geocode("punggol, singapore")
 end = ox.geocode("horizon primary school, singapore")
@@ -20,120 +21,120 @@ nodes, edges = ox.graph_to_gdfs(graph)
 # TO CREATE BUS ROUTING
 pathcheck = lrt(lrtGraph, graph, start, end, start_node, end_node)
 
-if pathcheck is not None:
-		# IF BUS ROUTE IS AVAILABLE
-		if pathcheck[1] == 0:
-			startStopCoords = pathcheck[2]
-			endStopCoords = pathcheck[3]
+# if pathcheck is not None:
+# 		# IF BUS ROUTE IS AVAILABLE
+# 		if pathcheck[1] == 0:
+# 			startStopCoords = pathcheck[2]
+# 			endStopCoords = pathcheck[3]
 
-			start_Lrt = ox.get_nearest_node(graph, startStopCoords)
-			end_Lrt = ox.get_nearest_node(graph, endStopCoords)
+# 			start_Lrt = ox.get_nearest_node(graph, startStopCoords)
+# 			end_Lrt = ox.get_nearest_node(graph, endStopCoords)
 
-			pathToLrt = astar_path(graph, start_node, start_Lrt)
-			pathFromLrt = astar_path(graph, end_Lrt, end_node)
+# 			pathToLrt = astar_path(graph, start_node, start_Lrt)
+# 			pathFromLrt = astar_path(graph, end_Lrt, end_node)
 
-			latlontoLrt = []
-			latlonfromLrt = []
+# 			latlontoLrt = []
+# 			latlonfromLrt = []
 
-			api = osm.OsmApi()  # this instantiate the OsmApi class,
+# 			api = osm.OsmApi()  # this instantiate the OsmApi class,
 
-			for item in pathToLrt:
-				node = api.NodeGet(item)
-				latlontoLrt.append((node["lat"], node["lon"]))
+# 			for item in pathToLrt:
+# 				node = api.NodeGet(item)
+# 				latlontoLrt.append((node["lat"], node["lon"]))
 
-			for item in pathFromLrt:
-				node = api.NodeGet(item)
-				latlonfromLrt.append((node["lat"], node["lon"]))
+# 			for item in pathFromLrt:
+# 				node = api.NodeGet(item)
+# 				latlonfromLrt.append((node["lat"], node["lon"]))
 
-			path = pathcheck[0]
-			indexing = 0
-			line = []
-			prevService = None
-			prevIndex = None
-			i = 0
-			markers = []
-			while i < len(path):
-				stopCode, service = path[i]
-				# in the case of first stop, no bus service stated, take next
-				if service is None:
-					service = path[i + 1][1]
+# 			path = pathcheck[0]
+# 			indexing = 0
+# 			line = []
+# 			prevService = None
+# 			prevIndex = None
+# 			i = 0
+# 			markers = []
+# 			while i < len(path):
+# 				stopCode, service = path[i]
+# 				# in the case of first stop, no bus service stated, take next
+# 				if service is None:
+# 					service = path[i + 1][1]
 
-				if service != prevService:
-					indexing = 0
+# 				if service != prevService:
+# 					indexing = 0
 
-				qlat = stop_desc_map[stopCode]["Latitude"]
-				qlon = stop_desc_map[stopCode]["Longitude"]
+# 				qlat = stop_desc_map[stopCode]["Latitude"]
+# 				qlon = stop_desc_map[stopCode]["Longitude"]
 
-				# get routes for respective direction
-				if service[1] == 1:
-					routing = LrtRoute0[service[0]]["coordinates"]
-				else:
-					routing = LrtRoute1[service[0]]["coordinates"]
-				while indexing < len(routing):
-					clon, clat = routing[indexing]
-					u = (qlat, qlon)
-					v = (clat, clon)
-					# stop found in range of 30 meters, latlong accuracy difference from
-					# two sources
-					if geopy.distance.distance(u, v).km < 0.03:
-						# first bus stop
-						if prevService is None:
-							line.append(v)
-						else:
-							if prevService == service:
-								for x, y in routing[prevIndex: indexing + 1]:
-									line.append((y, x))
-							else:
-								prevLatLong = line[-1]
-								tempIndex = 0
-								while tempIndex < len(routing):
-									plon, plat = routing[tempIndex]
-									p = (plat, plon)
-									if geopy.distance.distance(prevLatLong, p).km < 0.03:
-										for x, y in routing[tempIndex: indexing + 1]:
-											line.append((y, x))
-										break
-									tempIndex += 1
-						prevIndex = indexing
-						prevService = service
-						markers.append((v, stopCode))
-						break
-					indexing += 1
-				i += 1
-			# TO CREATE ROUTING WITH BUS
-			nodepath = astar_path(graph, start_node, end_node)
-			m = ox.plot_route_folium(
-				graph, nodepath, route_color='green', route_opacity=0)
-			folium.Marker(location=(start[0], start[1]), popup='START', icon=folium.Icon(
-				color='red', icon='flag')).add_to(m)
-			folium.Marker(location=(end[0], end[1]), popup='END', icon=folium.Icon(color='blue', icon='flag')).add_to(m)
-			for loc, code in markers:
-				folium.Marker(location=loc, popup='Lrt stop:' + str(code),
-							icon=folium.Icon(color='green', icon='train', prefix='fa')).add_to(m)
-			folium.PolyLine(line, color="red", weight=2.5, opacity=1).add_to(m)
+# 				# get routes for respective direction
+# 				if service[1] == 1:
+# 					routing = LrtRoute0[service[0]]["coordinates"]
+# 				else:
+# 					routing = LrtRoute1[service[0]]["coordinates"]
+# 				while indexing < len(routing):
+# 					clon, clat = routing[indexing]
+# 					u = (qlat, qlon)
+# 					v = (clat, clon)
+# 					# stop found in range of 30 meters, latlong accuracy difference from
+# 					# two sources
+# 					if geopy.distance.distance(u, v).km < 0.03:
+# 						# first bus stop
+# 						if prevService is None:
+# 							line.append(v)
+# 						else:
+# 							if prevService == service:
+# 								for x, y in routing[prevIndex: indexing + 1]:
+# 									line.append((y, x))
+# 							else:
+# 								prevLatLong = line[-1]
+# 								tempIndex = 0
+# 								while tempIndex < len(routing):
+# 									plon, plat = routing[tempIndex]
+# 									p = (plat, plon)
+# 									if geopy.distance.distance(prevLatLong, p).km < 0.03:
+# 										for x, y in routing[tempIndex: indexing + 1]:
+# 											line.append((y, x))
+# 										break
+# 									tempIndex += 1
+# 						prevIndex = indexing
+# 						prevService = service
+# 						markers.append((v, stopCode))
+# 						break
+# 					indexing += 1
+# 				i += 1
+# 			# TO CREATE ROUTING WITH BUS
+# 			nodepath = astar_path(graph, start_node, end_node)
+# 			m = ox.plot_route_folium(
+# 				graph, nodepath, route_color='green', route_opacity=0)
+# 			folium.Marker(location=(start[0], start[1]), popup='START', icon=folium.Icon(
+# 				color='red', icon='flag')).add_to(m)
+# 			folium.Marker(location=(end[0], end[1]), popup='END', icon=folium.Icon(color='blue', icon='flag')).add_to(m)
+# 			for loc, code in markers:
+# 				folium.Marker(location=loc, popup='Lrt stop:' + str(code),
+# 							icon=folium.Icon(color='green', icon='train', prefix='fa')).add_to(m)
+# 			folium.PolyLine(line, color="red", weight=2.5, opacity=1).add_to(m)
 
-			# start point to start busstop
-			folium.PolyLine([start, latlontoLrt[0]], color="blue", weight=2.5, opacity=1, dasharray="4").add_to(m)
+# 			# start point to start busstop
+# 			folium.PolyLine([start, latlontoLrt[0]], color="blue", weight=2.5, opacity=1, dasharray="4").add_to(m)
 
-			folium.PolyLine(latlontoLrt, color="green", weight=2.5, opacity=1).add_to(m)
+# 			folium.PolyLine(latlontoLrt, color="green", weight=2.5, opacity=1).add_to(m)
 
-			folium.PolyLine([latlontoLrt[-1], line[0]], color="green", weight=2.5, opacity=1, dasharray="4").add_to(m)
+# 			folium.PolyLine([latlontoLrt[-1], line[0]], color="green", weight=2.5, opacity=1, dasharray="4").add_to(m)
 
-			# End  bus stop to end point
-			folium.PolyLine([line[-1], latlonfromLrt[0]], color="green", weight=2.5, opacity=1, dasharray="4").add_to(m)
+# 			# End  bus stop to end point
+# 			folium.PolyLine([line[-1], latlonfromLrt[0]], color="green", weight=2.5, opacity=1, dasharray="4").add_to(m)
 
-			folium.PolyLine(latlonfromLrt, color="green", weight=2.5, opacity=1, dasharray="4").add_to(m)
+# 			folium.PolyLine(latlonfromLrt, color="green", weight=2.5, opacity=1, dasharray="4").add_to(m)
 
-			folium.PolyLine([latlonfromLrt[-1], end], color="blue", weight=2.5, opacity=1, dasharray="4").add_to(m)
+# 			folium.PolyLine([latlonfromLrt[-1], end], color="blue", weight=2.5, opacity=1, dasharray="4").add_to(m)
 
-			m.save('index.html')
+# 			m.save('index.html')
 
-		# IF BUS ROUTE NOT FOUND, RUN WALK ROUTE
-		if pathcheck[1] == 1:
-			nodepath = pathcheck[0]
-			m = ox.plot_route_folium(
-				graph, nodepath, route_color='green')
-			folium.Marker(location=(start[0], start[1]), popup='START', icon=folium.Icon(
-				color='red', icon='flag')).add_to(m)
-			folium.Marker(location=(end[0], end[1]), popup='END', icon=folium.Icon(color='blue', icon='flag')).add_to(m)
-			m.save('index.html')
+# 		# IF BUS ROUTE NOT FOUND, RUN WALK ROUTE
+# 		if pathcheck[1] == 1:
+# 			nodepath = pathcheck[0]
+# 			m = ox.plot_route_folium(
+# 				graph, nodepath, route_color='green')
+# 			folium.Marker(location=(start[0], start[1]), popup='START', icon=folium.Icon(
+# 				color='red', icon='flag')).add_to(m)
+# 			folium.Marker(location=(end[0], end[1]), popup='END', icon=folium.Icon(color='blue', icon='flag')).add_to(m)
+# 			m.save('index.html')
