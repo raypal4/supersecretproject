9967306c23c9d706ce9503103e34fcc06a686bd9
itxKAE <55869051+itxKAE@users.noreply.github.com>
2020-03-31 00:14:08 +0800

    added bus and lrt

---
+++ mapper/mapper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MapperConfig(AppConfig):
+    name = 'mapper'
---
+++ mapper/mapper/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.views.generic.base import TemplateView
+from mapper import views
+
+urlpatterns = [
+	path('index/', views.index),
+	path('dijskra/', TemplateView.as_view(template_name='dijskra_route.html')),
+	path('pdijskra/', TemplateView.as_view(template_name='pdijskra_route.html')),
+	path('astar/', TemplateView.as_view(template_name='astar_route.html')),
+	path('bus/', TemplateView.as_view(template_name='bus_routing.html')),
+	path('lrt/', TemplateView.as_view(template_name='lrt_routing.html')),
+]
